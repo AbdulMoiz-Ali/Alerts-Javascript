12e06f831f7ddd965ca1fd873b16f624020f1608
AbdulMoiz-Ali <abdulmoizali321@gmail.com>
2024-06-25 21:18:39 +0500

    alert

---
+++ app.js
@@ -0,0 +1,5 @@
+alert("Error! Please enter a valid password")
+alert("Welcome to JS Land...\rHappy coding!")
+alert("Welcome to JS land...")
+alert("Happy cooding!\rprevent this pagefrom creating additional dialogs.")
+alert("Hello..I can run JS through my web browser console")
